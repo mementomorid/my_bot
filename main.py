c4ee36424509a4d6b7f6d2ed96083d9c87f205de
mementomorid <6123@heemang.caees.kr>
2024-12-14 20:57:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from nextcord import *
 from nextcord.ext import commands
 import json
 import random
-import play2048 as p_2048
 from datetime import datetime, timedelta
 import time
 import math
@@ -13,7 +12,6 @@ from bs4 import BeautifulSoup
 from nextcord.ui import Button, View
 
 
-CUSTOM = p_2048.customs.ORIGINAL.value
 
 intents = nextcord.Intents.all()
 bot = commands.Bot(intents=intents)
@@ -369,130 +367,6 @@ async def search_long_words(ctx, 시작단어: str=nextcord.SlashOption(descript
     else:
         await ctx.send(f'"{시작단어}"로 시작하는 긴 단어가 없습니다.')
 
-class Play2048s(ui.View):
-    def __init__(self , comps:list[ui.Button], _2048:p_2048.Game, inter:Union[Interaction, commands.context.Context]):
-        super().__init__(timeout = 300)
-        for comp in comps:
-            self.add_item(comp)
-        self.inter = inter
-        self._2048 = _2048
-            
-    async def on_timeout(self) -> None:
-        with open("best2048.json", "r") as f: j:dict = json.load(f)
-        if type(self.inter) == Interaction:
-            if self._2048.point > j.get(str(self.inter.user.id), [0])[0]:
-                j[str(self.inter.user.id)] = [self._2048.point, int(time.time())]
-            
-            await self.inter.edit_original_message(view = None)
-        else:
-            if self._2048.point > j.get(str(self.inter.author.id), [0])[0]:
-                j[str(self.inter.author.id)] = [self._2048.point, int(time.time())]
-            
-            await self.inter.edit_original_message(view = None)
-        with open("best2048.json", "w") as f: json.dump(j, f, indent = 4)
-
-class Play2048(ui.Button):
-    def __init__(self, *, type:str = "img", user:Member = None, _2048:p_2048.Game = None, style:ButtonStyle = ButtonStyle.secondary, label:Optional[str] = "ㅤ", disabled:bool = False, custom_id:Optional[str] = None, url:Optional[str] = None, emoji:Optional[Union[str, Emoji, PartialEmoji]] = None, row:Optional[int] = None):
-        if user != None:
-            super().__init__(style = style, label = label, disabled = disabled, custom_id = f"{custom_id}|{user.id}", url = url, emoji = emoji, row = row)
-        else:
-            super().__init__(style = style, label = label, disabled = disabled, custom_id = custom_id, url = url, emoji = emoji, row = row)
-        self._2048 = _2048
-        self._type = type
-        
-    async def callback(self, inter:Interaction):
-        if self.custom_id.split("|")[1] != str(inter.user.id): return await inter.response.send_message("자신의것을 사용하세요!", ephemeral = True)
-        await inter.response.defer()
-        m = 0
-        if self.custom_id.startswith("up"):    m  = p_2048.move.UP
-        if self.custom_id.startswith("down"):  m  = p_2048.move.DOWN
-        if self.custom_id.startswith("left"):  m  = p_2048.move.LEFT
-        if self.custom_id.startswith("right"): m  = p_2048.move.RIGHT
-        
-        if m != 0:
-            self._2048.move(m)
-            with open("best2048.json", "r") as f: j:dict = json.load(f)
-            msg = " | **BEST SCORE!**" if self._2048.point > j.get(str(inter.user.id), [0])[0] else ""
-            
-            if self._type == "emj" or self._type == "emoji" or self._type == "이모지":
-                with open("emoji2048.json", "r") as f: emoji2048:dict = json.load(f)
-                _emj = self._2048.arr
-                emj = ""
-                for i in _emj:
-                    for j in i:
-                        emj += emoji2048[str(j)]
-                    emj += "\n"
-                    
-                
-                embed = Embed(title = f"점수: **{self._2048.point}점**", color = Color.blue())
-                await inter.message.edit(emj, embed = embed)
-                
-            else:
-                img = File(self._2048.encodingImage(CUSTOM).image_bytes, f"point_{self._2048.point}.png")
-            
-                await inter.message.edit(f"점수: **{self._2048.point}점**{msg}", file = img)
-                
-                
-
-        if self.custom_id.startswith("end"):
-            with open("best2048.json", "r") as f: j:dict = json.load(f)
-            if self._2048.point > j.get(str(inter.user.id), [0])[0]:
-                j[str(inter.user.id)] = [self._2048.point, int(time.time())]
-            
-                with open("best2048.json", "w") as f: json.dump(j, f, indent = 4)
-            await inter.message.edit(view = None)
-            await rank2048(inter)
-
-@bot.slash_command(description = "2048을 플레이합니다", name = "2048")
-async def play2048(inter:Interaction, type:str = SlashOption(description = "플레이할것을 선텍하세요", choices={"이미지": "img", "이모지":"emj"}, required=False)):
-    _2048 = p_2048.Game()
-    views = [
-        Play2048(disabled = True), Play2048(label = "w", style = ButtonStyle.blurple, user = inter.user, _2048 = _2048, custom_id="up", type=type), Play2048(disabled = True), Play2048(label = "end", style = ButtonStyle.red, user = inter.user, _2048 = _2048, custom_id="end", type=type),
-        Play2048(label = "a", style = ButtonStyle.blurple, user = inter.user, _2048 = _2048, row = 2, custom_id="left", type=type), Play2048(label = "s", style = ButtonStyle.blurple, user = inter.user, _2048 = _2048, row = 2, custom_id="down", type=type), Play2048(label = "d", style = ButtonStyle.blurple, user = inter.user, _2048 = _2048, row = 2, custom_id="right", type=type), Play2048(disabled = True, row = 2)
-    ]
-    if type == "img" or type == None:
-        await inter.response.send_message(f"점수: **0점**", file = File(_2048.encodingImage(CUSTOM).image_bytes, f"point_0.png"), view = Play2048s(views, _2048, inter))
-    else:
-        with open("emoji2048.json", "r") as f: emoji2048:dict = json.load(f)
-        _emj = _2048.arr
-        emj = ""
-        for i in _emj:
-            for j in i:
-                emj += emoji2048[str(j)]
-            emj += "\n"
-        
-        embed = Embed(title = f"점수: **0점**", color = Color.blue)
-        await inter.response.send_message(emj, embed = embed, view = Play2048s(views, _2048, inter))
-        
-@bot.slash_command(name = "2048랭크", description = "2048의 랭크를 확인합니다")
-async def rank2048(inter:Interaction):
-    try: await inter.response.defer()
-    except: ...
-    with open("best2048.json", "r") as f: _j:dict = json.load(f)
-    
-    j = sorted(_j.items() , key=lambda x : x[1][0] , reverse = True)
-    i = 1
-    description = ""
-    isShow = False
-    for rank in j[:10]:
-        user = utils.get(bot.get_all_members() , id = int(rank[0]))
-        description += f"{i}등, `{user}`: **{rank[1][0]}점** <t:{rank[1][1]}:R>\n"
-        i += 1
-        
-        if not isShow: isShow = inter.user == user
-        
-    
-    if not isShow:
-        i = 1
-        description += f".\n"*3
-        for rank in j:
-            if inter.user.id == int(rank[0]):
-                user = utils.get(bot.get_all_members() , id = int(rank[0]))
-                description += f"{i}등, `{user}`: **{rank[1][0]}점** <t:{rank[1][1]}:R>\n"
-                break
-            i += 1
-    embed = Embed(title = "2048 rank!", description = description, color = Color.blue())
-    await inter.followup.send(embed = embed)
 
 @bot.slash_command(name="로꾸꺼", description="문자열을 거꾸로 출력합니다.")
 async def reverse_text(interaction: Interaction, text: str = SlashOption(name="문자열",description="문자열을 입력해주세요.")):
